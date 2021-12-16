ca6eea4fcd2e68cf2af6581eac4f7c0b50b8e164
majeed <majeed.dl@gmail.com>
2021-12-16 09:59:04 +0300

    init javascript for node projects

---
+++ javascript/variables.js
@@ -0,0 +1,26 @@
+
+/* There are 3 ways to declare a Javascript variable
+ Using var
+ Using let 
+ Using const
+*/
+
+
+/*  Declare var */
+
+// It's a good programming practice to declare all variables at the begining of script
+var x = 5
+
+// One statement , Many Variables
+var person = "John Doe", carName = "Volvo", price = 200;
+
+// A variable decalre without a value will have the value "undefined"
+var carName;
+
+
+/* Declare let */
+let x = 12;
+
+
+/* Decalre const */
+const
