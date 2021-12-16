23035415ebc769284a3dc8b03ce1eea657103efc
majeed <majeed.dl@gmail.com>
2021-12-16 19:23:26 +0300

    work on types

--- typescript/types.ts
+++ typescript/types.ts
@@ -1,4 +1,5 @@
 
+//Basics
 
 let a: string = "Hello"
 
@@ -8,4 +9,26 @@ let c: boolean = true
 
 let d: Array<number> = [1, 2, 3];;
 
-let e: number[] = [ 20 ,30]
+let e: number[] = [20, 30];
+
+let f: any = "Hello World";
+
+//Functions
+// Parameter type and return type for functions
+const sampleFunction = (param: number): number => {
+    return param + 10
+}
+
+// The parameter's type annotation is an object type
+const functionTwo = (arg: { x: number, y: number }): number => {
+    return arg.x + arg.y
+}
+
+//Union Types
+let g: string | number = 12;
+
+
+export {
+    sampleFunction,
+    functionTwo
+}
