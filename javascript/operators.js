e920aed1870ad148b66a082bf8239688068a3443
majeed <majeed.dl@gmail.com>
2021-12-16 10:18:41 +0300

    add primary operators

---
+++ javascript/operators.js
@@ -0,0 +1,28 @@
+
+
+/* Operators */
+
+
+// Addition
+var a = b+c;
+
+// Subtraction
+var a = b-c;
+
+// Multiplication
+var a = b*c;
+
+// Exponentiation (ES2016)
+var a = b**c;
+
+// Division
+var a = b/c;
+
+// Modulus ( Division Remainder)
+var a = b%c;
+
+// Increment
+var a = b++;
+
+// Decrement
+var a = b--;
