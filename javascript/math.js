43df183db4c61a13f2a99b87d26ca5c0616213c8
majeed <majeed.dl@gmail.com>
2021-12-20 13:43:11 +0300

    add math decriptions

---
+++ javascript/math.js
@@ -0,0 +1,36 @@
+
+
+
+/*
+
+Math
+
+Number to Integer
+    There are 4 common methods to round a number to an integer:
+
+        Math.round(x)	Returns x rounded to its nearest integer
+        Math.ceil(x)	Returns x rounded up to its nearest integer
+        Math.floor(x)	Returns x rounded down to its nearest integer
+        Math.trunc(x)	Returns the integer part of x (new in ES6)
+
+
+Math.sign()
+    Math.sign(x) returns if x is negative, null or positive:
+
+
+Math.pow()
+    Math.pow(x, y) returns the value of x to the power of y:
+
+Math.sqrt()
+    Math.sqrt(x) returns the square root of x:
+
+Math.abs()
+    Math.abs(x) returns the absolute (positive) value of x:
+
+Math.random()
+    Math.random() returns a random number between 0 (inclusive), and 1 (exclusive):
+
+    Returns a random integer from 0 to 9:
+        Math.floor(Math.random() * 10);     
+
+*/
