d45eaff5d71c14a47edd95a9c8f19715fbe8a844
majeed <majeed.dl@gmail.com>
2021-12-20 14:02:24 +0300

    break

---
+++ javascript/break.js
@@ -0,0 +1,17 @@
+
+
+
+/*
+
+JavaScript Break and Continue
+
+
+The Break Statement
+    You have already seen the break statement used in an earlier chapter of this tutorial. It was used to "jump out" of a switch() statement.
+
+    The break statement can also be used to jump out of a loop
+
+
+The Continue Statement
+    The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop
+*/
