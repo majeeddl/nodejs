a50d7490f94998c2e8698d0851a86df1bf73a358
majeed <majeed.dl@gmail.com>
2022-01-20 22:42:59 +0300

    error handeling in tdd

--- api-tdd/app.test.js
+++ api-tdd/app.test.js
@@ -16,8 +16,29 @@ describe(" Todo API", () => {
             }),
           ])
         );
-      }).catch((err) =>{
-        
-      });
+      })
+      .catch((err) => {});
+  });
+
+  it("GET /todo --> object todo", () => {
+    request(app)
+      .get("/todos/1")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual(
+          expect.arrayConatining([
+            expect.arrayConatining({
+              name: expect.any(String),
+              completed: expect.any(Boolean),
+            }),
+          ])
+        );
+      })
+      .catch((err) => {});
+  });
+
+  it("GET /todo/id --> error 404 if not found", () => {
+    request(app).get("/todos/999").expect(404);
   });
 });
--- api-tdd/routes/todos.js
+++ api-tdd/routes/todos.js
@@ -1,11 +1,21 @@
 const express = require("express");
-
+const createError = require("http-errors");
 const router = express.Router();
 
 const todos = [{ id: 1, name: "test", completed: true }];
 
 router.get("/", (req, res) => {
   res.json(todos);
+});
+
+router.get("/:id", (req, res, next) => {
+  const foundTodo = todos.find((todo) => todo.id === Number(req.params.id));
+
+  if (!foundTodo) {
+    return next(createError(404, "Not Found"));
+  }
+
+  res.json(foundTodo);
 });
 
 module.exports = router;
