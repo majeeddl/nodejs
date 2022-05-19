9bfb477286cd3a32fcacdd3d0955124b9488a892
Majeed <majeed.dl@gmail.com>
2022-05-19 15:32:38 +0300

    change nest js project

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,32 +1,10 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { LoggerMiddleware } from 'src/middlewares/logger.middleware';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthorModule } from './modules/authors/author.module';
-import { HelloModule } from './modules/hello/hello.module';
-import { PipeModule } from './modules/pipes/pipe.module';
 
 @Module({
-  imports: [
-    AuthorModule,
-    // GraphQLModule.forRoot<ApolloDriverConfig>({
-    //   driver: ApolloDriver,
-    // }),
-    // GraphQLModule.forRoot({
-    //   playground: true,
-    //   installSubscriptionHandlers: true,
-    //   autoSchemaFile: 'schema.gql',
-    // }),
-    HelloModule,
-    PipeModule
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes();
-  }
-}
+export class AppModule {}
---
+++ nestjs_old/src/app.module.ts
@@ -0,0 +1,33 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { LoggerMiddleware } from 'src/middlewares/logger.middleware';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthorModule } from './modules/authors/author.module';
+import { HelloModule } from './modules/hello/hello.module';
+import { PipeModule } from './modules/pipes/pipe.module';
+import { CatsController } from './cats/cats.controller';
+
+@Module({
+  imports: [
+    AuthorModule,
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    // }),
+    // GraphQLModule.forRoot({
+    //   playground: true,
+    //   installSubscriptionHandlers: true,
+    //   autoSchemaFile: 'schema.gql',
+    // }),
+    HelloModule,
+    PipeModule
+  ],
+  controllers: [AppController, CatsController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes();
+  }
+}
