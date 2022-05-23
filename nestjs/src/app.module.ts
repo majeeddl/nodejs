cf0f6857140bb425de720cb6920bd05afcab3e6f
Majeed <majeed.dl@gmail.com>
2022-05-23 22:05:05 +0300

    add swagger

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path : 'cats' , method : RequestMethod.GET
+    })
+  }
+}
