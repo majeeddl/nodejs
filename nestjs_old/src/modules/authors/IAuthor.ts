41cc36dcb43536fefbf8978dd4cd69394898184d
Majeed <majeed.dl@gmail.com>
2022-05-24 11:18:42 +0300

    add design project projects

--- nestjs_old/src/modules/authors/IAuthor.ts
+++
@@ -1,8 +0,0 @@
-
-// export interface Author {
-//   id: Int!
-//   firstName: String
-//   lastName: String
-//   posts: [Post!]!
-// }
-
--- nestjs_old/src/modules/pipes/custom.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
-
-
-@Injectable()
-export class CustomPipe implements PipeTransform{
-    transform(value: any, metadata: ArgumentMetadata) {
-        return value.length
-    }
-}
