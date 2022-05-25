adbc584ec247c538be7477a0391258b5badbffe4
Majeed <majeed.dl@gmail.com>
2022-05-25 15:52:52 +0300

    exception filters are implemented

--- nestjs/src/cats/cats.controller.ts
+++ nestjs/src/cats/cats.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Req, Param, Post, Body, Put } from '@nestjs/common';
+import { Controller, Get, Req, Param, Post, Body, Put, HttpException, HttpStatus, UseFilters } from '@nestjs/common';
 import { Request } from 'express';
+import { ForbiddenException } from 'src/exceptions/forbidden.exception';
+import { HttpExceptionFilter } from 'src/exceptions/http-exception.filter';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './create-cat.dto';
 import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
-
-  constructor(private catsService:CatsService){}
+  constructor(private catsService: CatsService) {}
 
   @Get()
   async findAll(@Req() request: Request = null): Promise<Cat[]> {
@@ -27,5 +28,32 @@ export class CatsController {
   @Put()
   async update(@Param('id') id: string, @Body() updateCatDto: any) {
     return 'update cat';
+  }
+
+  @Get('exception')
+  async handleException() {
+    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+  }
+
+  @Get('exception/message')
+  async handleExceptionCustomMessage() {
+    throw new HttpException(
+      {
+        status: HttpStatus.FORBIDDEN,
+        error: 'this is a custom message',
+      },
+      HttpStatus.FORBIDDEN,
+    );
+  }
+
+  @Get('exception/custom')
+  async handleExceptionWithCustomException() {
+    throw new ForbiddenException();
+  }
+
+  @Get('exception/filter')
+  @UseFilters(new HttpExceptionFilter())
+  async exceptionFilter() {
+    throw new ForbiddenException();
   }
 }
