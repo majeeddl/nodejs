8654e5f354d8bb71324b62772b93149bc6eba8b5
Majeed Dourandeesh <majeed.dl@gmail.com>
2024-05-12 02:21:54 +0300

    create a simple websocket server

---
+++ websockets/index.js
@@ -0,0 +1,12 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+
+    ws.send("your message was: " + data + " - thanks!");
+  });
+  ws.send("something");
+});
